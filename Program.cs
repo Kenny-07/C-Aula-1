3da76e1d2a453f744d334aa0da7735464b5204ae
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-11-11 22:02:20 -0300

    Hello World!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
